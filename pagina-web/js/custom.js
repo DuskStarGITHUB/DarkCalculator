5718f30ff78403aced293c7f96f810299bfa59a1
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-08-03 12:23:25 -0600

    JS FIX

--- pagina-web/js/custom.js
+++ pagina-web/js/custom.js
@@ -3,12 +3,3 @@ function openNav() {
   document.getElementById("myNav").classList.toggle("menu_width");
   document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 }
-
-// AÑO DE FOOTER AUTOMATIZADO
-function getCurrentYear() {
-  var currentDate = new Date();
-  var currentYear = currentDate.getFullYear();
-  document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getCurrentYear();
