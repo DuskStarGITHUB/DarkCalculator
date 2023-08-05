d3c38501007dd93c0798ef976b009a3c389261a8
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-08-04 18:52:21 -0600

    Update Documents

--- pagina-web/js/extra.js/docuemnts.js
+++ pagina-web/js/extra.js/docuemnts.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll(".content section");
-    const links = document.querySelectorAll(".sidebar a");
+  const sections = document.querySelectorAll(".content section");
+  const links = document.querySelectorAll(".sidebar a");
 
-    links.forEach((link, index) => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            sections.forEach(section => {
-                section.style.display = "none";
-            });
-            sections[index].style.display = "block";
-        });
+  links.forEach((link, index) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      sections.forEach((section) => {
+        section.style.display = "none";
+      });
+      sections[index].style.display = "block";
     });
+  });
 });
