613adb089eee8bcfcb8f15007a9755043eb57b9f
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-05 16:19:42 -0600

    funcion conversor temperatura

--- Dark-Calculator/pack/conversor_operations.py
+++ Dark-Calculator/pack/conversor_operations.py
@@ -27,3 +27,13 @@ def convertir_longitud(valor, unidad_origen, unidad_destino):
     # Retornar el resultado
     return valor_convertido
 
+def convertir_temperatura(valor, unidad_origen, unidad_destino):
+    """Sistema: temperatura"""
+    conversiones = {
+        'C': (1.0, 0.0),
+        'F': (1.8, 32.0)
+    }
+    factor_origen, desplazamiento_origen = conversiones[unidad_origen]
+    factor_destino, desplazamiento_destino = conversiones[unidad_destino]
+    valor_convertido = (valor - desplazamiento_origen) * factor_destino / factor_origen + desplazamiento_destino
+    return valor_convertido
