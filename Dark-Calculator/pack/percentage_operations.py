08c4f1b7fbac1b6f555c0e462b47260548d02ef7
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-05 16:35:37 -0600

    INIT

---
+++ Dark-Calculator/pack/percentage_operations.py
@@ -0,0 +1,7 @@
+"""Titulo: |Operations|"""
+#####################################################################################
+# Descripción: <Funciones de operacion de aumento o reduccion porcentual>
+# autor: <DuskStar>
+# Fecha de creación: <05/09/2023>
+# Nota: <N/A>
+#####################################################################################
