92730b2b5439e57ca4b73c7ee966643c533ec9fb
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-05 13:26:37 -0600

    Operaciones basicas

--- Dark-Calculator/pack/basic_operations.py
+++ Dark-Calculator/pack/basic_operations.py
@@ -5,3 +5,36 @@
 # Fecha de creación: <04/09/2023>
 # Nota: <N/A>
 #####################################################################################
+
+def suma(num_1,num_2):
+    """Función que suma dos numeros"""
+    return num_1+num_2
+
+def resta(num_1,num_2):
+    """Función que resta dos numeros"""
+    return num_1-num_2
+
+def multiplicacion(num_1,num_2):
+    """Función que multiplica dos numeros"""
+    return num_1*num_2
+
+def division(num_1,num_2):
+    """Función que divide dos numeros"""
+    if num_2 == 0:
+        raise ValueError("Division por cero")
+    else:
+        return num_1/num_2
+
+def potencinum_1(num_1,num_2):
+    """Función que eleva un numero a otro"""
+    if num_2 < 0:
+        raise ValueError("Potencia negativa")
+    else:
+        return num_1**num_2
+
+def porcentaje(num_1,num_2):
+    """Función que calcula el porcentaje de un numero"""
+    if num_2 < 0:
+        raise ValueError("Porcentaje negativo")
+    else:
+        return num_1*num_2/100
