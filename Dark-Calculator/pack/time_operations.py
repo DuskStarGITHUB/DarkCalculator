82c332ae08d7a6eb56d0fa0803fa3ddff2fc7d5e
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-06 13:48:16 -0600

    POO

--- Dark-Calculator/pack/time_operations.py
+++ Dark-Calculator/pack/time_operations.py
@@ -5,3 +5,43 @@
 # Fecha de creación: <06/09/2023>
 # Nota: <N/A>
 #####################################################################################
+
+class CalculadoraTiempo:
+    """Estructura del objeto"""
+    def __init__(self):
+        self.horas = 0
+        self.minutos = 0
+        self.segundos = 0
+
+    def ingresar_tiempo(self, horas, minutos, segundos):
+        """Tiempo"""
+        self.horas = horas
+        self.minutos = minutos
+        self.segundos = segundos
+
+    def sumar_tiempo(self, horas, minutos, segundos):
+        """Operacion suma"""
+        self.horas += horas
+        self.minutos += minutos
+        self.segundos += segundos
+        self.normalizar_tiempo()
+
+    def restar_tiempo(self, horas, minutos, segundos):
+        """Operacion resta"""
+        self.horas -= horas
+        self.minutos -= minutos
+        self.segundos -= segundos
+        self.normalizar_tiempo()
+
+    def normalizar_tiempo(self):
+        """Normaliza los valores de los atributos"""
+        if self.segundos >= 60:
+            self.minutos += self.segundos // 60
+            self.segundos %= 60
+        if self.minutos >= 60:
+            self.horas += self.minutos // 60
+            self.minutos %= 60
+
+    def obtener_resultado(self):
+        """Devuelve el resultado"""
+        return self.horas, self.minutos, self.segundos
