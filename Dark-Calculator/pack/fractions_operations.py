195eac870ec0ab00da654e6d7d3e2a41ad651f17
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-06 13:41:49 -0600

    Objeto de calculadora de fracciones

---
+++ Dark-Calculator/pack/fractions_operations.py
@@ -0,0 +1,28 @@
+"""Titulo: |Operations|"""
+#####################################################################################
+# Descripción: <Funciones de operacion de operaciones basicas con fracciones>
+# autor: <DuskStar>
+# Fecha de creación: <05/09/2023>
+# Nota: <N/A>
+#####################################################################################
+
+from fractions import Fraction
+
+class FraccionCalculadora:
+    """Estructura del objeto"""
+    def __init__(self, numerador, denominador, operacion):
+        self.fraccion = Fraction(numerador, denominador)
+        self.operacion = operacion
+    def realizar_operacion(self, otra_fraccion):
+        """Funcion de operaciones"""
+        if self.operacion == '+':
+            resultado = self.fraccion + otra_fraccion
+        elif self.operacion == '-':
+            resultado = self.fraccion - otra_fraccion
+        elif self.operacion == '*':
+            resultado = self.fraccion * otra_fraccion
+        elif self.operacion == '/':
+            resultado = self.fraccion / otra_fraccion
+        else:
+            raise ValueError("Operación no válida")
+        return resultado
