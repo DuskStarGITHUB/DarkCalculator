e5609d8862b22b430c79616759fcb5ccb62799ac
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-09-06 13:51:21 -0600

    INIT

---
+++ Dark-Calculator/pack/cav_operaations.py
@@ -0,0 +1,9 @@
+"""Titulo: |Operations|"""
+#####################################################################################
+# Descripción: <Funciones de operaciones de calculos de area y volumen>
+# autor: <DuskStar>
+# Fecha de creación: <06/09/2023>
+# Nota: <N/A>
+#####################################################################################
+
+# POO
